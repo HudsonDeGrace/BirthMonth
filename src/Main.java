848ea9d92f762cf0ee618d02a84a297296a92ec7
Hudson Degrace <5degraceh@LU-CTE-AD.local>
2024-09-26 09:09:23 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int month = 0;
+        String trash = "";
+
+        System.out.println("What is your birth month [Enter a number 1-12]");
+        if(in.hasNextInt()){
+            month = in.nextInt();
+            in.nextLine();
+            if(month > 0 && month < 13){
+                System.out.println("Your birth month is: " + month);
+            } else {
+                System.out.println("You entered an incorrect month value: " + month);
+            }
+        } else {
+            trash = in.nextLine();
+            System.out.println("\nYou said your birth month was: " + trash);
+            System.out.println("Run the program again and enter a valid amount!");
+        }
+    }
+}
